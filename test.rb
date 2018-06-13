099b934d52b5d6825b00354460c205f337a8071c
cocowaszak <colettewaszak@gmail.com>
2018-06-13 18:18:16 +0000

    automatically backed up by learn

---
+++ test.rb
@@ -0,0 +1,42 @@
+#age = 9
+#if age < 10
+#puts "You are too young. BYE!"
+#end
+#if age == 9
+#puts "Welcome!"
+#end
+
+#age = 15
+#if age >18
+#puts "Welcome"
+#else 
+#puts "Too Young"
+#end
+
+#Create a if else program that displays passed if if someone got a grade of a 70 or higher and otherwise puts failed
+
+#grade = 40
+#if grade >= 70
+ # puts "You passed"
+#else 
+#  puts "You failed"
+#end
+
+#a = gets.chomp.to_i
+#if a>10
+#  puts "Your number is greater than 10"
+#else 
+#  puts "Your number is not greater than 10"
+#end 
+
+#if Time.now.hour >=7 && Time.now.hour <=11
+ # puts "Good Morning!"
+ # elsif Time.now.hour >=11 && Time.now.hour <=17
+ # puts "Good Afternoon!"
+#elsif Time.now.hour >=17 && Time.now.hour <=22
+#puts "Good Evening!"
+#elsif Time.now.hour >= 22
+#puts "Good Night!"
+#end 
+  
+  
